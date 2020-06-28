35821333d2997e5d2d75d24154627ceb6478bc6d
Joey Schrader <jschrader@overstock.com>
2020-06-28 16:51:01 -0600

    complete toggleTodo logic

--- src/App/TodoList/List/Item/Item.js
+++ src/App/TodoList/List/Item/Item.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+/** styles */
+import styles from './Item.module.css'
+
 export const Item = ({
     item,
     deleteTodo,
@@ -7,7 +10,7 @@ export const Item = ({
     id,
 }) => {
     return (
-        <li>
+        <li className={`${styles.item} ${item.completed ? styles.completed : ''}`}>
             <h4>{item.title}</h4>
             <button onClick={() => toggleTodo(id)}>Edit</button>
             <button onClick={() => deleteTodo(id)}>Delete</button>
